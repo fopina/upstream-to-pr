bc3e05de668d2995234e5d517828121cd73bfdfc
Filipe Pina <fopina@gmail.com>
2023-03-23 22:45:09 +0000

    x

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const token: string = core.getInput('token')
+    // const token: string = core.getInput('token')
     const upstreamRepository: string = core.getInput('upstream-repository')
-    const upstreamBranch: string = core.getInput('upstream-branch')
+    // const upstreamBranch: string = core.getInput('upstream-branch')
 
     // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-    core.info(`Waiting ${upstreamRepository} milliseconds ...`) 
+    core.info(`Waiting ${upstreamRepository} milliseconds ...`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
