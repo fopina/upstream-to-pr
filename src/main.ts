cdec2dfcee7c36a369bab02b3473f0df38043b1a
Filipe Pina <fopina@gmail.com>
2023-03-23 22:56:11 +0000

    x

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as io from '@actions/io'
 
 async function run(): Promise<void> {
   try {
@@ -8,6 +10,18 @@ async function run(): Promise<void> {
 
     // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
     core.info(`Waiting ${upstreamRepository} milliseconds ...`)
+    core.debug(`Debug test`)
+
+    // const stdout: string[] = []
+    const options = {
+    }
+    const gitPath = await io.which('git', true)
+
+    const exitCode = await exec.exec(`"${gitPath}"`, ["status"], options)
+    //result.stdout = stdout.join('')
+
+    core.debug(exitCode.toString())
+    //core.debug(stdout)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
