57ffd951dd3bf6bbdb8941c41da783d877a8a5e5
Filipe Pina <636320+fopina@users.noreply.github.com>
2023-03-24 01:30:51 +0000

    refactor to typescript action (#2)

---
+++ __tests__/main.test.ts
@@ -0,0 +1,22 @@
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
+import {expect, test} from '@jest/globals'
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  process.env['INPUT_TOKEN'] = 'xxx'
+  process.env['INPUT_UPSTREAM-REPOSITORY'] = '.'
+  process.env['INPUT_UPSTREAM-BRANCH'] = 'develop'
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  try {
+    console.log(cp.execFileSync(np, [ip], options).toString())
+    expect(true).toBe(false)
+  } catch (e: any) {
+    expect(e.stdout.toString()).toContain('Checking .@develop for changes ...')
+  }
+})
---
+++ src/main.ts
@@ -0,0 +1,109 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import * as io from '@actions/io'
+
+async function run(): Promise<void> {
+  try {
+    const token: string = core.getInput('token', {required: true})
+    const upstreamRepository: string = core.getInput('upstream-repository', {
+      required: true
+    })
+    let upstreamBranch: string = core.getInput('upstream-branch') || 'main'
+    const context = github.context
+    let currentBranch = context.ref || ''
+
+    if (upstreamBranch.startsWith('refs/')) {
+      upstreamBranch = upstreamBranch.substring(5)
+    }
+    if (currentBranch.startsWith('refs/')) {
+      currentBranch = currentBranch.substring(5)
+    }
+
+    core.info(
+      `Checking ${upstreamRepository}@${upstreamBranch} for changes ...`
+    )
+
+    await execGit(['fetch', upstreamRepository, upstreamBranch])
+
+    const revList = (
+      await execGit(['rev-list', `${currentBranch}..FETCH_HEAD`])
+    ).stdout.trim()
+    // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.debug(`revList: [${revList}]`)
+    if (!revList) {
+      core.info('Nothing new here, move along.')
+      return
+    }
+
+    const revHead = (
+      await execGit(['rev-parse', '--short', 'FETCH_HEAD'])
+    ).stdout.trim()
+    const branch = `upstream-to-pr/rev-${revHead}`
+
+    // check if branch already exists - this require a clone with full fetch depth
+    // `fetch-depth: 0` in github checkout action
+    const branches = await execGit(['branch', '-a'])
+    if (branches.stdout.includes(`${branch}\n`)) {
+      core.info('Branch already exists, skipping')
+      return
+    }
+
+    await execGit(['checkout', '-b', branch, 'FETCH_HEAD'])
+    await execGit(['push', '-u', 'origin', branch])
+
+    const octokit = github.getOctokit(token)
+    const {data: pullRequest} = await octokit.rest.pulls.create({
+      ...context.repo,
+      title: `Upstream revision ${revHead}`,
+      head: branch,
+      base: currentBranch,
+      body: `Auto-generated pull request.`
+    })
+    core.info(`Pull request created: ${pullRequest.url}`)
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+async function execGit(
+  args: string[],
+  allowAllExitCodes = false,
+  silent = false,
+  customListeners = {}
+): Promise<GitOutput> {
+  // borrowed from actions/checkout - https://github.com/actions/checkout/blob/main/src/git-command-manager.ts
+
+  const gitPath = await io.which('git', true)
+  const result = new GitOutput()
+
+  const defaultListener = {
+    stdout: (data: Buffer) => {
+      stdout.push(data.toString())
+    }
+  }
+
+  const mergedListeners = {...defaultListener, ...customListeners}
+
+  const stdout: string[] = []
+  const options = {
+    silent,
+    ignoreReturnCode: allowAllExitCodes,
+    listeners: mergedListeners
+  }
+
+  result.exitCode = await exec.exec(`"${gitPath}"`, args, options)
+  result.stdout = stdout.join('')
+
+  core.debug(result.exitCode.toString())
+  core.debug(result.stdout)
+
+  return result
+}
+
+class GitOutput {
+  stdout = ''
+  exitCode = 0
+}
+
+run()
