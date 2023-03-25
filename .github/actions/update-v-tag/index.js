d4f9ebbd490f9ff2ddf348c7c899d99ec447dedf
Filipe Pina <fopina@gmail.com>
2023-03-25 18:04:49 +0000

    x

--- .github/actions/update-v-tag/index.js
+++ .github/actions/update-v-tag/index.js
@@ -1,1 +1,16 @@
 console.log("hello")
+
+const { spawn } = require('node:child_process');
+const ls = spawn('git', ['tag', '-l']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
