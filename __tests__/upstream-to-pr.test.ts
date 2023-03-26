cd28c740f63aef5a2c9572b72ad52774ac2342e0
Filipe Pina <fopina@gmail.com>
2023-03-26 14:57:06 +0100

    x

--- __tests__/upstream-to-pr.test.ts
+++ __tests__/upstream-to-pr.test.ts
@@ -165,7 +165,7 @@ describe('test upstream-to-pr update-tag', () => {
   const firstInfoLine =
     'Checking http://github.com/god/world.git for newer tags...'
   const octoMock = github.getOctokit('x')
-  const createMock = jest.spyOn(octoMock, 'request').mockResolvedValue({
+  const requestMock = jest.spyOn(octoMock, 'request').mockResolvedValue({
     status: 200,
     data: [
       {
@@ -194,8 +194,8 @@ describe('test upstream-to-pr update-tag', () => {
     expect(mInfo).toHaveBeenNthCalledWith(2, 'Updating to tag v1.10.1...')
     // fetch
     expect(mExec).toBeCalledTimes(1)
-    expect(createMock).toHaveBeenCalledTimes(1)
-    expect(createMock).toHaveBeenCalledWith('GET /repos/god/world/tags')
+    expect(requestMock).toHaveBeenCalledTimes(1)
+    expect(requestMock).toHaveBeenCalledWith('GET /repos/god/world/tags')
   })
 
   it('fetches any tag', async () => {
@@ -211,7 +211,78 @@ describe('test upstream-to-pr update-tag', () => {
     expect(mInfo).toHaveBeenNthCalledWith(2, 'Updating to tag v1.12.1-dev...')
     // fetch
     expect(mExec).toBeCalledTimes(1)
+    expect(requestMock).toHaveBeenCalledTimes(1)
+    expect(requestMock).toHaveBeenCalledWith('GET /repos/god/world/tags')
+  })
+
+  it('skips on missing match', async () => {
+    await new UpstreamToPr(
+      'http://github.com/god/world.git',
+      'main',
+      'xXx',
+      'main',
+      'v3..*'
+    ).fetchHEAD()
+    expect(mInfo).toBeCalledTimes(2)
+    expect(mInfo).toHaveBeenNthCalledWith(1, firstInfoLine)
+    expect(mInfo).toHaveBeenNthCalledWith(
+      2,
+      'No matching tags found, ignoring.'
+    )
+    expect(mExec).not.toHaveBeenCalled()
+    expect(requestMock).toHaveBeenCalledTimes(1)
+    expect(requestMock).toHaveBeenCalledWith('GET /repos/god/world/tags')
+  })
+
+  it('creates PR if tag is new', async () => {
+    process.env['GITHUB_REPOSITORY'] = 'xxx'
+    mExec.mockImplementation(async (...args) => {
+      if (args[1]) {
+        switch (args[1][0]) {
+          case 'rev-list':
+            args[2]?.listeners?.stdout!(Buffer.from('x'))
+            break
+          case 'rev-parse':
+            args[2]?.listeners?.stdout!(Buffer.from('bababa'))
+            break
+          case 'branch':
+            args[2]?.listeners?.stdout!(Buffer.from('upstream-to-pr/rev-\n'))
+            break
+        }
+      }
+      return 0
+    })
+    const createMock = jest
+      .spyOn(octoMock.rest.pulls, 'create')
+      .mockResolvedValue({
+        data: {
+          url: 'http://git.url/to/pr'
+        }
+      } as any)
+    await new UpstreamToPr(
+      'http://github.com/god/world.git',
+      'main',
+      'xXx',
+      'main',
+      'v.*'
+    ).run()
+    // fetch, rev-list, rev-parse, branch, checkout, push
+    expect(mExec).toBeCalledTimes(6)
+    expect(mInfo).toBeCalledTimes(3)
+    expect(mInfo).toHaveBeenNthCalledWith(1, firstInfoLine)
+    expect(mInfo).toHaveBeenNthCalledWith(2, 'Updating to tag v1.12.1-dev...')
+    expect(mInfo).toHaveBeenNthCalledWith(
+      3,
+      'Pull request created: http://git.url/to/pr.'
+    )
     expect(createMock).toHaveBeenCalledTimes(1)
-    expect(createMock).toHaveBeenCalledWith('GET /repos/god/world/tags')
+    expect(createMock).toHaveBeenCalledWith({
+      base: 'main',
+      body: 'Auto-generated pull request.',
+      head: 'upstream-to-pr/rev-bababa',
+      owner: 'xxx',
+      repo: undefined,
+      title: 'Upstream revision bababa'
+    })
   })
 })
