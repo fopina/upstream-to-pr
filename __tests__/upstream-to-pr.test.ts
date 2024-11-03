d21dbcde4fc567dbf89399537773a73013c95a46
Filipe Pina <hzlu1ot0@duck.com>
2024-11-03 01:01:25 +0000

    x

--- __tests__/upstream-to-pr.test.ts
+++ __tests__/upstream-to-pr.test.ts
@@ -214,7 +214,7 @@ describe('test upstream-to-pr update-tag', () => {
   const firstInfoLine =
     'Checking http://github.com/god/world.git for newer tags...'
   const octoMock = github.getOctokit('x')
-  const requestMock = jest.spyOn(octoMock, 'request').mockResolvedValue({
+  const requestMock = jest.spyOn(octoMock, 'listTags').mockResolvedValue({
     status: 200,
     data: [
       {
